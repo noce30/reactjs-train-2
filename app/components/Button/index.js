9fa9d47cbbb8d7a512d8d4ff6a2b656f4cdca7af
darrenpham <darren.pham@unicorn.vn>
2018-03-20 14:37:03 +0700

    -clear project

---
+++ app/components/Button/index.js
@@ -0,0 +1,27 @@
+/**
+*
+* Button
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
+
+class Button extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        <FormattedMessage {...messages.header} />
+      </div>
+    );
+  }
+}
+
+Button.propTypes = {
+
+};
+
+export default Button;
